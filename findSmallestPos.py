4023acd545293d02f07b8eb542af2d0ae52f92d0
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2025-12-20 00:36:27 +0530

    fixed changes

--- findSmallestPos.py
+++ findSmallestPos.py
@@ -1,14 +1,13 @@
-class Solution:
-    def firstMissingPositive(self, nums):
-        n = len(nums)
+nums=[3,2,1,7,11,2]
+def smallestPos(nums):
+    n=len(nums)
+    for i in range (0,n):
+        while 1<=nums[i]<=n and nums[nums[i]-1]!=nums[i]:
+            correct_index=nums[i]-1
+            nums[i], nums[correct_index] = nums[correct_index], nums[i]
+    for i in range (0,n):
+        if nums[i]!=i+1:
+            return i+1
 
-        for i in range(n):
-            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-                correct_index = nums[i] - 1
-                nums[i], nums[correct_index] = nums[correct_index], nums[i]
-
-        for i in range(n):
-            if nums[i] != i + 1:
-                return i + 1
-
-        return n + 1
+    return n+1
+print(smallestPos(nums))
