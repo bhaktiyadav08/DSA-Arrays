9a9892c55fb19e7ac77b3786cb13c38a9ac9d13f
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-01-16 23:46:18 +0530

    minimum time to visit all points

--- Arrays/minTimeToVisitAllPoints.py
+++ Arrays/minTimeToVisitAllPoints.py
@@ -2,8 +2,8 @@ points = [[1,1],[3,4],[-1,0]]
 def minTimeToVisitAllPoints(points):
     time=0
     for i in range (1,len(points)):
-        dx=points[i-1][1]-points[i][0]
-        dy=points[i-1][1]-points[i][0]
+        dx=abs(points[i-1][0]-points[i][0])
+        dy=abs(points[i-1][1]-points[i][1])
         time+=max(dx,dy)
     return time
 print(minTimeToVisitAllPoints(points))
