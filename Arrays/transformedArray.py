b21c45d4f3d940e8fc829c5f6be76a2548e3c362
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-02-13 23:16:09 +0530

    transformed Array

--- Arrays/transformedArray.py
+++ Arrays/transformedArray.py
@@ -1,5 +1,4 @@
-class Solution:
-    def constructTransformedArray(self, nums):
+def transformedArray(nums):
         n = len(nums)
         return [nums[((i + nums[i]) % n + n) % n] for i in range(n)]
-        
+print(transformedArray([1,-1,7,4]))
