d6815a6d7febc23db0d0a19b603c239e10ee22be
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-02-17 23:22:49 +0530

    reverse bits

--- Arrays/reverseBits.py
+++ Arrays/reverseBits.py
@@ -1,10 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
-        
         for i in range(32):
             result <<= 1           # Make space for next bit
             result |= n & 1        # Add least significant bit of n
             n >>= 1                # Remove least significant bit from n
-        
         return result
