6a2367cb2ca3a9322875b4437f318ed36c2be433
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-03-02 23:33:17 +0530

    concatenated binary numbers

---
+++ Math/concatenatedBinary.py
@@ -0,0 +1,14 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10**9 + 7
+        ans = 0
+        bit_length = 0
+        for i in range(1, n + 1):
+            # If i is a power of 2, the number of bits increases
+            if i & (i - 1) == 0:
+                bit_length += 1
+            
+            # Shift the result left by bit_length and add i
+            ans = ((ans << bit_length) | i) % MOD
+            
+        return ans
